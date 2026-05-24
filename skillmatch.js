d7a65c5d6043280f5ae92b12aa06c8b1ca204f0e
KarinaMVieira <karina_m_vieira@estudante.sesisenai.org.br>
2026-05-23 21:51:38 -0300

    Código inicial.

---
+++ skillmatch.js
@@ -0,0 +1,33 @@
+const candidato = {
+    nome: "Júlia",
+    area: "Front-End",
+    habilidades: ["JavaScript", "Github", "Lógica de Programação", "Kanban"],
+    experienciaMeses: 4
+};
+
+const vaga = [
+    {
+    id:1,
+    empresa: "TechStart",
+    cargo: "Desenvolvedor Front-End Júnior",
+    requisitos: ["JavaScript", "Github", "Lógica de Programação"],
+    salario: 2800,
+    modalidade: "Remoto"
+    },
+    {
+    id:2,
+    empresa: "CodeLab",
+    cargo: "Estágio Front-End",
+    requisitos: ["JavaScript", "React", "Github", "Kanban"],
+    salario: 1800,
+    modalidade: "Híbrido"
+    },
+    {
+    id:3,
+    empresa: "WebSolutions",
+    cargo: "Programador JavaScript Júnior",
+    requisitos: ["JavaScript", "Arrays", "Objeto", "Funções", "Html"],
+    salario: 3000,
+    modalidade: "Presencial"
+    }
+];
